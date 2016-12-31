f8ad0c63895ffe54e022c2f9ba85cde282496c7a
MadhuAdiga <madhu.vah@gmail.com>
2016-12-31 13:51:17 +0530

    Assignment4

---
+++ js/app.js
@@ -0,0 +1,60 @@
+
+var employee1= { //Declaring employee object
+	name: "Marion J. Turner",
+	age:35,
+	salary:10000,
+	address: {
+			city: "Hudsonville",
+			state: "MI",
+			pincode: "49426"
+	}
+};
+
+var employee2= { //Declaring employee object
+	name: "Dottie K. Williams",
+	age:66,
+	salary:11000,
+	address: {
+			city: "Hialeah",
+			state: "FL",
+			pincode: "33012"
+	}
+};
+
+
+var employee3= { //Declaring employee object
+	name: "John W. Jablonski",
+	age:66,
+	salary:11200,
+	address: {
+			city: "Grand Rapids",
+			state: "MI",
+			pincode: "49503"
+	}
+};
+
+var employee4= { //Declaring employee object
+	name: "Mindy B. Brown",
+	age:83,
+	salary:7200,
+	address: {
+			city: "New Brunswick",
+			state: "NJ",
+			pincode: "08901"
+	}
+};
+
+var employee5= { //Declaring employee object
+	name: "Kevin J. Coury",
+	age:70,
+	salary:14600,
+	address: {
+			city: "New Ulm",
+			state: "MN",
+			pincode: "56073"
+	}
+};
+	
+var employee=[employee1,employee2,employee3,employee4,employee5];//Declaring employee array with the 5 objects as array elements
+console.log(employee[2]); //Publishing the third element of the array to the console
+	
